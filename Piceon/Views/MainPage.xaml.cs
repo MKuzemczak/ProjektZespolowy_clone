ffee153b487073d57fe538a12b220defd094da59
MKuzemczak <kuzemczak.michal@gmail.com>
2020-03-09 12:26:38 +0100

    copied  treeview into main page

--- Piceon/Views/MainPage.xaml.cs
+++ Piceon/Views/MainPage.xaml.cs
@@ -1,16 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
+using Piceon.Core.Models;
+using Piceon.Core.Services;
+
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+using WinUI = Microsoft.UI.Xaml.Controls;
 
 namespace Piceon.Views
 {
     public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
+        private object _selectedItem;
+
+        public object SelectedItem
+        {
+            get { return _selectedItem; }
+            set { Set(ref _selectedItem, value); }
+        }
+
+        public ObservableCollection<SampleCompany> SampleItems { get; } = new ObservableCollection<SampleCompany>();
+
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var data = await SampleDataService.GetTreeViewDataAsync();
+            foreach (var item in data)
+            {
+                SampleItems.Add(item);
+            }
+        }
+
+        private void OnItemInvoked(WinUI.TreeView sender, WinUI.TreeViewItemInvokedEventArgs args)
+            => SelectedItem = args.InvokedItem;
+
+        private void OnCollapseAll(object sender, RoutedEventArgs e)
+            => CollapseNodes(treeView.RootNodes);
+
+        private void CollapseNodes(IList<WinUI.TreeViewNode> nodes)
+        {
+            foreach (var node in nodes)
+            {
+                CollapseNodes(node.Children);
+                treeView.Collapse(node);
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
