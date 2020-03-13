9d32fc88f5ba90e36c03055d4dbadf6aaf4a7d87
MKuzemczak <kuzemczak.michal@gmail.com>
2020-03-13 08:37:36 +0100

    TreeView now displays the folder tree of user's picture library

---
+++ Piceon/Models/DirectoryItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Windows.Storage;
+
+namespace Piceon.Models
+{
+    public class DirectoryItem
+    {
+        public StorageFolder Folder { get; set; }
+        public List<DirectoryItem> Subdirectories { get; set; } = new List<DirectoryItem>();
+
+    }
+}
