4fe907ebb9b856b0583f5dbbde2c5c634096da9f
maciek666 <czerwoneszelki19@gmail.com>
2020-03-25 00:19:35 +0100

    przygotowanie argumentow wywolania do dwoch selectow

--- PythonScripts/db/query_service.py
+++ PythonScripts/db/query_service.py
@@ -1,5 +1,5 @@
 class QueryService:
-    def prepare_args_to_call_select(self, argv):
+    def prepare_args_to_call_select(self, argv) -> str:
         if len(argv) >= 3:
             comparator = '('
             args = [arg for arg in argv if arg != ' ']
@@ -9,3 +9,19 @@ class QueryService:
             return comparator
         else:
             return 'no_argv'
+
+    def prepare_args_to_two_selct(self, argv) -> (str, str):
+        """"
+            SQLite nie zwraca w zadanej obiektów, więc similar_images.py potrzebuje wysłać dwa selecty
+            zeby otrzymac glowny obrazek oraz porownywane
+        """
+        if len(argv) < 3:
+            return 'no_argv'
+        c1 = '(' + str(argv[1]) + ')'
+        args = [arg for arg in argv if arg != ' ']
+        c2 = '('
+        for index in range(2, len(args) - 1):
+            c2 = c2 + str(args[index]) + ', '
+        c2 = c2 + str(args[len(args) - 1]) + ')'
+
+        return c1, c2
