9d21aad84ed191304c1700d3eddcf955e3d093a6
Michał Kuzemczak <kuzemczak.michal@gmail.com>
2020-05-24 18:34:31 +0200

    pre merge

---
+++ Piceon/Controls/WarningMark.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Piceon.Controls
+{
+    public sealed partial class WarningMark : UserControl, INotifyPropertyChanged
+    {
+        public bool IsVisible
+        {
+            get { return (bool)GetValue(IsVisibleProperty); }
+            set
+            {
+                SetValue(IsVisibleProperty, value);
+                UpdateCheckMarkVisibility();
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for IsVisible.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsVisibleProperty =
+            DependencyProperty.Register("IsVisible", typeof(bool), typeof(WarningMark), new PropertyMetadata(0));
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public WarningMark()
+        {
+            this.InitializeComponent();
+        }
+
+        private void UpdateCheckMarkVisibility()
+        {
+            warningMarkImage.Visibility = IsVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
